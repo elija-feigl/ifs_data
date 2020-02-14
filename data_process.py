4eb3f2b0a7c5e2c60b025c9c9daff8a5114ced32
elija-feigl <elija.feigl@gmail.com>
2020-02-14 13:22:31 +0100

    data_process.py: small updates:

--- data_process.py
+++ data_process.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import designData
 from pathlib import Path
 
@@ -11,27 +10,26 @@ def export_data(data: dict, name: str) -> None:
     export_str = ", ".join([str(i) for i in export.values()])
 
     try:
-        os.mkdir("/home/kourosh/Documents/designdata/outcome")
+        os.mkdir("./database")
     except FileExistsError:
         pass
 
     try:
-        with open("./outcome/" + "-designdata.csv", mode="r+") as out:
+        with open("./database/designdata.csv", mode="r+") as out:
             if header == out.readline(0):
                 pass
     except FileNotFoundError:
-        with open("./outcome/" + "-designdata.csv", mode="w+") as out:
+        with open("./database/designdata.csv", mode="w+") as out:
             out.write(header + "\n")
 
-    with open("./outcome/" + "-designdata.csv", mode="a") as out:
+    with open("./database/designdata.csv", mode="a") as out:
         out.write(export_str + "\n")
         # out.write("\nEND")
     return
 
 
 def main():
-    folders = Path("Data/")
-    i = 0
+    folders = Path("./")
     for folder in folders.iterdir():
         for files in folder.iterdir():
             if files.suffix == ".txt":
@@ -41,10 +39,6 @@ def main():
                         if line.startswith('Project ='):
                             name = line[9:-1].strip()
                             break
-
-    # print("master, I am awaiting the name of your design")
-    # name = input()
-    # print("Thank you Sir")
 
                     designdata = designData.DesignData(name=name)
                     data = designdata.compute_data()
